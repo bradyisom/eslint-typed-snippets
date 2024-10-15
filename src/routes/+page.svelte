bfca1b496c9ce0eab2053f33bce8d22d892f77ff
Brady Isom <bradyisom@gmail.com>
2024-10-15 11:28:55 -0600

    Initial commit showcasing eslint bug

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	function hello() {
+		console.log('Hello');
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{@render editor_button({ onclick: hello })}
+
+<!-- prettier-ignore -->
+{#snippet editor_button(foo: {
+    onclick: () => void,
+})}
+  <button on:click={foo.onclick}>Click me</button>
+{/snippet}
